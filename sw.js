ae1258be6a68bc39945dc099134845fe74a93608
pepperparker8 <iqbal.reza@gmail.com>
2026-05-08 07:11:17 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = 'vitalcore-v2';
+const FILES = ['/vitalcore/', '/vitalcore/index.html', '/vitalcore/manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
